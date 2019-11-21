78080ba62720e8e38b77149c2fc120c44da558be
illbexyz <albyx.n@gmail.com>
2019-11-21 20:48:29 +0100

    Added a script to rebuild the lib and generate the bindings

---
+++ generate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+# Parameter: the folder 
+bindings_lib=$1
+
+if [ "$bindings_lib" == "" ]; then
+    echo "Error: missing argument \$bindings_lib (the name one of the folders inside bindings/)"
+    exit 1
+fi
+
+# Configure the build if it's the first time
+if [ ! -d "./dist" ]
+then
+    runhaskell Setup.hs configure --user
+fi
+
+# Build and register the compiled library
+runhaskell Setup.hs build
+runhaskell Setup.hs install
+
+# If the bindings haven't 
+if [ ! -d "./dist/$bindings_lib/dist" ]
+then
+    pushd bindings
+        cabal new-run genBuildInfo Atk
+        pushd $bindings_lib
+            cabal v1-install
+        popd
+    popd
+fi
+
+pushd bindings/$bindings_lib
+    rm -rf GI dist; cabal v1-build
+popd
